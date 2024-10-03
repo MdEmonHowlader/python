eec351ab92ac09017b0ee7b98297a4c272e13e39
Md. Emon <115633465+MdEmonHowlader@users.noreply.github.com>
2024-10-03 14:46:40 +0600

    Replace data

---
+++ replace.py
@@ -0,0 +1,4 @@
+def replace_fifth(text):
+    return text.replace("5th", "7th")
+string="You currentty studying 5th sem.."
+print(replace_fifth(string))
--- test.py
+++
@@ -1,6 +0,0 @@
-d=[181, 172, 152, 187, 351]
-def reverse_sort(data):
-    s=sorted(data)
-    re=s[::-1]
-    return re
-print(reverse_sort(d))
