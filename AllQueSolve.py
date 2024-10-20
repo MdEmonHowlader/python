9dab9569f1b22bf7a7430e906c885e2960652794
Md. Emon <115633465+MdEmonHowlader@users.noreply.github.com>
2024-10-21 00:48:57 +0600

    updata

--- AllQueSolve.py
+++ AllQueSolve.py
@@ -45,3 +45,12 @@ if len(lst)<3:
     print("Not possible")
 else:
     print(lst[1:-1])
+    
+    
+
+print("\n")
+i = 1
+while i <= 100:
+    if i % 2 != 0:
+        print(i)
+    i += 1
--- test.py
+++ test.py
@@ -1,49 +1,22 @@
-k=[101,102,103,104]
-v=["USA", "BD", "UK", "China"]
-lst= dict(zip(k, v))
-print(lst)
+# tup = (10, 20, 30, 40, 50)
+# value = int(input("Enter a value to search: "))
 
-# def calculate_waiver(cgpa, fee):
-#     if cgpa>3.50:
-#         waiver=0.20
-#     elif 3.00<=cgpa <=3.50:
-#         waiver=0.10
-#     else:
-#         waiver=0.5
-#     net_fee=fee-(fee*waiver)
-#     return net_fee
-# cgpa=float(input("Enter You CGPA: "))
-# fee=float(input("Enter Your Fee: "))
-# net_fee=calculate_waiver(cgpa, fee)
-# # print(net_fee)
+# if value in tup:
+#     print(f"Index of {value}: {tup.index(value)}")
+# else:
+#     print("Value not found in tuple.")
 
-def odd_number():
-    i = 1
-    odd_list = []
-    while i <= 100:
-        if i % 2 != 0:
-            odd_list.append(i)
-        i += 1
-    return odd_list
+squares = [x ** 2 for x in range(1, 11)]
 
-print(odd_number())
+for num in squares:
+    print(f"Square of {int(num ** 0.5)} is {num}")
 
-print("*\n")
-list1=[1,2,3]
-list2=[5,4,5]
-common=list(set(list1)& set(list2))
-if common:
-    print("Common elements are: ", common)
-else:
-    print("No common element's")
     
     
-print('\n')
+scores = {"Alice": 85, "Bob": 90, "Charlie": 80}
 
-lst=[2,3,4,5,6]
-if len(lst)<3:
-    print("Not possible")
-else:
-    print(lst[1:-1])
-        
+max_student = max(scores, key=scores.get)
+print(f"Top student: {max_student} with score {scores[max_student]}")
+
+    
             
